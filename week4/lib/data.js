dad5a4eecb7d805ad565f1bbd5bebbb367a4db99
Isaac m <67026968+TheCh33t@users.noreply.github.com>
2021-09-23 01:53:16 +0000

    first add to lib/data and [id]

--- week4/lib/data.js
+++ week4/lib/data.js
@@ -87,3 +87,55 @@ const jsonObj3 = jsonObj.concat(jsonObj2);
   return objReturned;
 }
 
+// called from [id].js getStaticProps() function
+export async function getData(idRequested) {
+  // get filepath to json file
+  const filePath = path.join(dataDir, 'persons.json');
+  const filePath2 = path.join(dataDir, 'relations.json');
+
+  // load json file contents
+  const jsonString = fs.readFileSync(filePath, 'utf8');
+  const jsonString2 = fs.readFileSync(filePath2, 'utf8');
+
+  // convert string from file into json array object
+  const jsonObj = JSON.parse(jsonString);
+  const jsonObj2 = JSON.parse(jsonString2);
+
+  // find object value in array that has matching id
+  const objMatch = jsonObj.filter(obj => {
+    return obj.id.toString() === idRequested;
+  });
+
+  // extract object value in filtered array if any
+  let objReturned;
+  if (objMatch.length > 0) {
+    objReturned = objMatch[0];
+
+    // find matching owner_id in relations data model
+    const objMatch2 = jsonObj2.filter(obj => {
+        return obj.owner_id.toString() === idRequested;
+      }
+    );
+
+    if (objMatch2.length > 0) {
+      // since we found an entry in relations, now let's find all the rows
+      // of persons that have id in the array of related_ids
+      const objMatch3 = jsonObj.filter(obj => {
+          return objMatch2[0].related_ids.includes( obj.id );
+        }
+      );
+
+      if (objMatch3.length > 0) {
+        objReturned.related = objMatch3;
+      }
+    }
+
+  } else {
+    objReturned = {};
+  }
+  
+  // console.log(objReturned);
+
+  // return object value found
+  return objReturned;
+}
